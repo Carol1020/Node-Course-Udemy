3b25981a86368817dc79eb2620ad6c9c43666947
CarolLiu <carol.gugu@hotmail.com>
2020-09-25 21:27:59 +1000

    making http request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,9 @@
-console.log("Starting");
+const request = require("request");
 
-setTimeout(() => {
-  console.log("2 Second Timer");
-}, 2000);
+const url =
+  "http://api.weatherstack.com/current?access_key=6caae61751610c4e6297eb66f8712ef7&query=37.8267,-122.4233";
 
-setTimeout(() => {
-  console.log("0 Second Timer");
-}, 0);
-
-console.log("Stopping");
+request({ url: url }, (error, response) => {
+  const data = JSON.parse(response.body);
+  console.log(data.current);
+});
