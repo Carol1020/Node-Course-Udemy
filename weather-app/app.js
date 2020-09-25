fb0828f477bbdc9d5031763f214f64a94f3b31d7
CarolLiu <carol.gugu@hotmail.com>
2020-09-25 22:00:04 +1000

    customizing HTTP Request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,21 @@
 const request = require("request");
 
 const url =
-  "http://api.weatherstack.com/current?access_key=6caae61751610c4e6297eb66f8712ef7&query=37.8267,-122.4233";
+  "http://api.weatherstack.com/current?access_key=6caae61751610c4e6297eb66f8712ef7&query=37.8267,-122.4233&units=f";
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+  const temp = response.body.current.temperature;
+  const feelslike = response.body.current.feelslike;
+  console.log(
+    response.body.current.weather_descriptions[0] +
+      ". It is currently " +
+      temp +
+      " degrees out. It feels like " +
+      feelslike +
+      " degrees out."
+  );
 });
+
+// Goal: Print a small forecast to the user
+// 1. Print: "It is currently 9 degrees out. It feels like 5 degrees out."
+// 2. Test your work!
