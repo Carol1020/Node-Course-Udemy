383ffd087b1095fba26e06916e3e7523a08def96
CarolLiu <carol.gugu@hotmail.com>
2020-10-01 09:27:51 +1000

    geocode as a constant in utils

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,26 +1,27 @@
 const request = require("request");
+const geocode = require("./utils/geocode");
 
-const url =
-  "http://api.weatherstack.com/current?access_key=6caae61751610c4e6297eb66f8712ef7&query=37.8267,-122.4233&units=f";
+// const url =
+//   "http://api.weatherstack.com/current?access_key=6caae61751610c4e6297eb66f8712ef7&query=37.8267,-122.4233&units=f";
 
-request({ url: url, json: true }, (error, response) => {
-  if (error) {
-    console.log("unable to connect to weather service!");
-  } else if (response.body.error) {
-    console.log("Unable to find location.");
-  } else {
-    const temp = response.body.current.temperature;
-    const feelslike = response.body.current.feelslike;
-    console.log(
-      response.body.current.weather_descriptions[0] +
-        ". It is currently " +
-        temp +
-        " degrees out. It feels like " +
-        feelslike +
-        " degrees out."
-    );
-  }
-});
+// request({ url: url, json: true }, (error, response) => {
+//   if (error) {
+//     console.log("unable to connect to weather service!");
+//   } else if (response.body.error) {
+//     console.log("Unable to find location.");
+//   } else {
+//     const temp = response.body.current.temperature;
+//     const feelslike = response.body.current.feelslike;
+//     console.log(
+//       response.body.current.weather_descriptions[0] +
+//         ". It is currently " +
+//         temp +
+//         " degrees out. It feels like " +
+//         feelslike +
+//         " degrees out."
+//     );
+//   }
+// });
 
 // Geocoding
 // Address -> Lat / Long -> weather
@@ -30,17 +31,22 @@ request({ url: url, json: true }, (error, response) => {
 // 3. Setup error handling for no matching results
 // 4. Test by altering the search term and running the app
 
-const geocodingURL =
-  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiY2Fyb2xsaXUxMDIwIiwiYSI6ImNrYzF0Z202NDB3dm0yd29lZjJuM2YwemYifQ.hUUReXyGMR8OF4QxfUh7kQ&limit=1";
+// const geocodingURL =
+//   "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiY2Fyb2xsaXUxMDIwIiwiYSI6ImNrYzF0Z202NDB3dm0yd29lZjJuM2YwemYifQ.hUUReXyGMR8OF4QxfUh7kQ&limit=1";
 
-request({ url: geocodingURL, json: true }, (error, response) => {
-  if (error) {
-    console.log("Unable to connect to Mapbox.");
-  } else if (response.body.features[0].length === 0) {
-    console.log("Unable to find location.");
-  } else {
-    const lat = response.body.features[0].center[0];
-    const long = response.body.features[0].center[1];
-    console.log(lat, long);
-  }
+// request({ url: geocodingURL, json: true }, (error, response) => {
+//   if (error) {
+//     console.log("Unable to connect to Mapbox.");
+//   } else if (response.body.features[0].length === 0) {
+//     console.log("Unable to find location.");
+//   } else {
+//     const lat = response.body.features[0].center[0];
+//     const long = response.body.features[0].center[1];
+//     console.log(lat, long);
+//   }
+// });
+
+geocode("Boston", (error, data) => {
+  console.log("Error", error);
+  console.log("Data", data);
 });
---
+++ weather-app/utils/geocode.js
@@ -0,0 +1,29 @@
+const request = require("request");
+
+const geocode = (address, callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    encodeURIComponent(address) +
+    ".json?access_token=pk.eyJ1IjoiY2Fyb2xsaXUxMDIwIiwiYSI6ImNrYzF0Z202NDB3dm0yd29lZjJuM2YwemYifQ.hUUReXyGMR8OF4QxfUh7kQ&limit=1";
+
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to location services!", undefined);
+    } else if (response.body.features.length === 0) {
+      callback("Unable to find location. Try another search.", undefined);
+    } else {
+    }
+    callback(undefined, {
+      latitude: response.body.features[0].center[0],
+      longtitude: response.body.features[0].center[1],
+      location: response.body.features[0].place_name,
+    });
+  });
+};
+
+geocode("Philadelphia", (error, data) => {
+  console.log("Error", error);
+  console.log("Data", data);
+});
+
+module.exports = geocode;
