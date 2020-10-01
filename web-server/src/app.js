27f337b8a8225a35ede8830b04767af8eb25e711
CarolLiu <carol.gugu@hotmail.com>
2020-10-02 09:37:34 +1000

    Serving up Static Assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,35 +1,25 @@
+const path = require("path");
 const express = require("express");
-const app = express();
 
-// app.com
-// app.com/help
-// app.com/about
+const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
+
+//
+// Goal: Create two more HTML files
+//
+// 1. Create a html page for about with "About" title
+// 2. Create a html page for  help with "Help" title
+// 3. Remove the old route  handlers for both
+// 4. Visit both in the browser to test your work
 
 app.get("", (req, res) => {
-  res.send("<h1>Weather</h1>");
-});
-
-app.get("/help", (req, res) => {
-  res.send([
-    {
-      name: "Andrew",
-    },
-    { age: 27 },
-  ]);
-});
-//
-//Goal: Update routes
-// 1. Setup about route to render a title with HTML
-// 2. Setup a weather route to send back JSON
-//    - Object with forecast and location strings
-// 3. Test your work by visiting both in the browser
-//
-app.get("/about", (req, res) => {
-  res.send("<h1>About page</h1>");
+  res.send();
 });
 
 app.get("/weather", (req, res) => {
-  res.send({ forecast: "Cloudy", location: "New York" });
+  res.send();
 });
 
 app.listen(3000, () => {
