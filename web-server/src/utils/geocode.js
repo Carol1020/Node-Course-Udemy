6cc0d52ec6f7ba523c38c927b61f17fd74d2f289
CarolLiu <carol.gugu@hotmail.com>
2020-10-08 08:59:41 +1100

    debugging

--- web-server/src/utils/geocode.js
+++ web-server/src/utils/geocode.js
@@ -14,8 +14,8 @@ const geocode = (address, callback) => {
     } else {
     }
     callback(undefined, {
-      latitude: body.features[0].center[0],
-      longtitude: body.features[0].center[1],
+      latitude: body.features[0].center[1],
+      longtitude: body.features[0].center[0],
       location: body.features[0].place_name,
     });
   });
