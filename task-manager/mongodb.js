c125673b2e7b09a0669b182f9233004b55007f53
CarolLiu <carol.gugu@hotmail.com>
2020-10-09 14:25:22 +1100

    The ObjectID

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,10 +1,17 @@
 // CRUD create Read Update Delete
 
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require("mongodb");
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+
+const { MongoClient, ObjectID } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
+
+const id = new ObjectID();
+console.log(id);
+console.log(id.getTimestamp());
 
 MongoClient.connect(connectionURL, { useNewParser: true }, (error, client) => {
   if (error) {
@@ -13,18 +20,19 @@ MongoClient.connect(connectionURL, { useNewParser: true }, (error, client) => {
 
   const db = client.db(databaseName);
 
-  // db.collection("users").insertOne(
-  //   {
-  //     name: "Carol",
-  //     age: 26,
-  //   },
-  //   (error, result) => {
-  //     if (error) {
-  //       return console.log("Unable to insert user");
-  //     }
-  //     console.log(result.ops);
-  //   }
-  // );
+  db.collection("users").insertOne(
+    {
+      _id: id,
+      name: "Vikram",
+      age: 29,
+    },
+    (error, result) => {
+      if (error) {
+        return console.log("Unable to insert user");
+      }
+      console.log(result.ops);
+    }
+  );
 
   // db.collection("users").insertMany(
   //   [
@@ -45,26 +53,26 @@ MongoClient.connect(connectionURL, { useNewParser: true }, (error, client) => {
   //   }
   // );
 
-  db.collection("documents").insertMany(
-    [
-      {
-        description: "This is document 1.",
-        completed: true,
-      },
-      {
-        description: "This is document 2.",
-        completed: tru,
-      },
-      {
-        description: "This is document 3.",
-        completed: false,
-      },
-    ],
-    (error, result) => {
-      if (error) {
-        return console.log("Invalid document");
-      }
-      console.log(result.ops);
-    }
-  );
+  // db.collection("documents").insertMany(
+  //   [
+  //     {
+  //       description: "This is document 1.",
+  //       completed: true,
+  //     },
+  //     {
+  //       description: "This is document 2.",
+  //       completed: tru,
+  //     },
+  //     {
+  //       description: "This is document 3.",
+  //       completed: false,
+  //     },
+  //   ],
+  //   (error, result) => {
+  //     if (error) {
+  //       return console.log("Invalid document");
+  //     }
+  //     console.log(result.ops);
+  //   }
+  // );
 });
