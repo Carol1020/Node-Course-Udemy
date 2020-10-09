03b5161a7b83e5fe0d3c641d59599f10bed4721f
CarolLiu <carol.gugu@hotmail.com>
2020-10-09 14:01:38 +1100

    Insert documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -13,11 +13,58 @@ MongoClient.connect(connectionURL, { useNewParser: true }, (error, client) => {
 
   const db = client.db(databaseName);
 
-  db.collection("users").insertOne(
-    {
-      name: "Carol",
-      age: 26,
-    },
-    (error, result) => {}
+  // db.collection("users").insertOne(
+  //   {
+  //     name: "Carol",
+  //     age: 26,
+  //   },
+  //   (error, result) => {
+  //     if (error) {
+  //       return console.log("Unable to insert user");
+  //     }
+  //     console.log(result.ops);
+  //   }
+  // );
+
+  // db.collection("users").insertMany(
+  //   [
+  //     {
+  //       name: "Jen",
+  //       age: 28,
+  //     },
+  //     {
+  //       name: "Gunther",
+  //       age: 27,
+  //     },
+  //   ],
+  //   (error, result) => {
+  //     if (error) {
+  //       return console.log("Unable to insert documents!");
+  //     }
+  //     console.log(result.ops);
+  //   }
+  // );
+
+  db.collection("documents").insertMany(
+    [
+      {
+        description: "This is document 1.",
+        completed: true,
+      },
+      {
+        description: "This is document 2.",
+        completed: tru,
+      },
+      {
+        description: "This is document 3.",
+        completed: false,
+      },
+    ],
+    (error, result) => {
+      if (error) {
+        return console.log("Invalid document");
+      }
+      console.log(result.ops);
+    }
   );
 });
