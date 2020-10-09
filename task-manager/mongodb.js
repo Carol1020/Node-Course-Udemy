4ac076910ff35c1b0ff3cd0daea420088905e93d
CarolLiu <carol.gugu@hotmail.com>
2020-10-09 14:55:50 +1100

    Querying Documents fetching data challenge

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -16,25 +16,40 @@ MongoClient.connect(connectionURL, { useNewParser: true }, (error, client) => {
 
   const db = client.db(databaseName);
 
-  db.collection("users").findOne(
-    { _id: new ObjectID("5f7fd5fb1f1e8c053187dd11") },
-    (error, user) => {
-      if (error) {
-        return console.log("Unable to fetch");
-      }
-      console.log(user);
+  // db.collection("users").findOne(
+  //   { _id: new ObjectID("5f7fd5fb1f1e8c053187dd11") },
+  //   (error, user) => {
+  //     if (error) {
+  //       return console.log("Unable to fetch");
+  //     }
+  //     console.log(user);
+  //   }
+  // );
+
+  // db.collection("users")
+  //   .find({ age: 26 })
+  //   .toArray((error, users) => {
+  //     console.log(users);
+  //   });
+
+  // db.collection("users")
+  //   .find({ age: 26 })
+  //   .count((error, count) => {
+  //     console.log(count);
+  //   });
+
+  db.collection("documents").findOne(
+    {
+      _id: new ObjectID("5f7fd193479db8ffd1735c19"),
+    },
+    (error, result) => {
+      console.log(result);
     }
   );
 
-  db.collection("users")
-    .find({ age: 26 })
-    .toArray((error, users) => {
-      console.log(users);
-    });
-
-  db.collection("users")
-    .find({ age: 26 })
-    .count((error, count) => {
-      console.log(count);
+  db.collection("documents")
+    .find({ completed: true })
+    .toArray((error, result) => {
+      console.log(result);
     });
 });
