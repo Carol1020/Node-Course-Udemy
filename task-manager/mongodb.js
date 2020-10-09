24f89be5581a588b1499be50fc514c4e6506eda2
CarolLiu <carol.gugu@hotmail.com>
2020-10-09 15:39:22 +1100

    deleteMany

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -16,74 +16,12 @@ MongoClient.connect(connectionURL, { useNewParser: true }, (error, client) => {
 
   const db = client.db(databaseName);
 
-  // db.collection("users").findOne(
-  //   { _id: new ObjectID("5f7fd5fb1f1e8c053187dd11") },
-  //   (error, user) => {
-  //     if (error) {
-  //       return console.log("Unable to fetch");
-  //     }
-  //     console.log(user);
-  //   }
-  // );
-
-  // db.collection("users")
-  //   .find({ age: 26 })
-  //   .toArray((error, users) => {
-  //     console.log(users);
-  //   });
-
-  // db.collection("users")
-  //   .find({ age: 26 })
-  //   .count((error, count) => {
-  //     console.log(count);
-  //   });
-
-  // db.collection("documents").findOne(
-  //   {
-  //     _id: new ObjectID("5f7fd193479db8ffd1735c19"),
-  //   },
-  //   (error, result) => {
-  //     console.log(result);
-  //   }
-  // );
-
-  // db.collection("documents")
-  //   .find({ completed: true })
-  //   .toArray((error, result) => {
-  //     console.log(result);
-  //   });
-
-  // db.collection("users")
-  //   .updateOne(
-  //     {
-  //       _id: new ObjectID("5f7fcb54d15921f9062c3db2"),
-  //     },
-  //     {
-  //       $inc: {
-  //         age: 1,
-  //       },
-  //     }
-  //   )
-  //   .then((result) => {
-  //     console.log(result);
-  //   })
-  //   .catch((error) => {
-  //     console.log(error);
-  //   });
-
-  db.collection("documents")
-    .updateMany(
-      {
-        completed: false,
-      },
-      {
-        $set: {
-          completed: true,
-        },
-      }
-    )
+  db.collection("users")
+    .deleteMany({
+      age: 27,
+    })
     .then((result) => {
-      console.log(result.modifiedCount);
+      console.log(result);
     })
     .catch((error) => {
       console.log(error);
