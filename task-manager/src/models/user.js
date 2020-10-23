57e2d868b4e0057e69cba227ee4fd8541001edc8
CarolLiu <carol.gugu@hotmail.com>
2020-10-23 12:43:55 +1100

    user model debug

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,11 +22,11 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    minlength: 7,
     trim: true,
-    minLength: 7,
     validate(value) {
       if (value.toLowerCase().includes("password")) {
-        throw new Error("Password can not contain password");
+        throw new Error('Password cannot contain "password"');
       }
     },
   },
@@ -35,17 +35,19 @@ const userSchema = new mongoose.Schema({
     default: 0,
     validate(value) {
       if (value < 0) {
-        throw new Error("Age must be a positive number");
+        throw new Error("Age must be a postive number");
       }
     },
   },
 });
 
-userSchema.pre("save", function (next) {
+userSchema.pre("save", async function (next) {
   const user = this;
+
   if (user.isModified("password")) {
-    user.password = await bcrypt.hash(user.password, 8)
+    user.password = await bcrypt.hash(user.password, 8);
   }
+
   next();
 });
 
